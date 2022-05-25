f97a7f6ef0db55c7998cab628442d3b25430925c
VenDiago <alizg@code.edu.az>
2022-05-25 09:45:17 +0400

    new

---
+++ main.js
@@ -0,0 +1,23 @@
+window.onload=function(){
+    
+    $(document).ready(function () {
+        $('#close-btn').click(function () {
+          $('#search-overlay').fadeOut();
+          $('#search-btn').show();
+        });
+        $('#search-btn').click(function () {
+          $(this).hide();
+          $('#search-overlay').fadeIn();
+        });
+      });
+
+
+
+
+
+
+
+
+
+
+}
